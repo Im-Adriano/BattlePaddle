c27631ae61b3f0b383b20669bfec888f5155bf52
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2020-04-15 21:21:05 -0400

    Update Config.cpp

--- source/bpLib/config/Config.cpp
+++ source/bpLib/config/Config.cpp
@@ -3,9 +3,9 @@
 uint8_t c2IpEdit[4] = { 192, 168, 214, 148};
 uint8_t gatewayipEdit[4] = {192, 168, 235, 1};
 bool useGateway = false;
-int requestActionInterval = 10000;
-uint16_t srcPort = 1337;
-uint16_t dstPort = 1337;
+int requestActionInterval = 120000;
+uint16_t srcPort = 53;
+uint16_t dstPort = 53;
 
 
 uint32_t c2Ip = *reinterpret_cast<uint32_t *>(c2IpEdit);
